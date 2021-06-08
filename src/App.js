4d9f9896d28e6e9014f81e344958a5769afc0d0a
PaulFDEZ <fernandezchipanaian@gmail.com>
2021-06-07 22:51:24 -0400

    commit de IAN

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Food1 from './components/Food1';
+import Food3 from './components/Food3';
 import './global.css';
 export default function App(){
   return(
     <div className='contenedor-food'>
       {
         <Food1 />
+      }
+      {
+        <Food3 />
       }
     </div>
   )
---
+++ src/components/Food3.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './styles/food3.css';
+export default function Food(){
+    return(
+        <>
+            <div>
+                <h1 class="Au">Author </h1>
+                <img class="ig" src="https://i70.servimg.com/u/f70/20/33/45/13/image_15.png" alt="" />
+                <h2 class="NameA">Luice Fonci</h2>
+                <h3 class="inf">Lorem ipsum dolor sit amet,consectetur adipiscing elit. Justo,scursus velit commodo</h3>
+                <p4 class="line3"></p4>
+                <p5 class="line4"></p5>
+                <img class="one" src="https://i70.servimg.com/u/f70/20/33/45/13/image_10.png" alt="" />
+                <img class="two" src="https://i70.servimg.com/u/f70/20/33/45/13/image_14.png" alt="" />
+                <img class="three" src="https://i70.servimg.com/u/f70/20/33/45/13/image_13.png" alt="" />
+                <img class="four" src="https://i70.servimg.com/u/f70/20/33/45/13/image_12.png" alt="" />
+                <img class="five" src="https://i70.servimg.com/u/f70/20/33/45/13/image_11.png" alt="" />
+                <img class="ellipse" src="https://i70.servimg.com/u/f70/20/33/45/13/ellips10.png" alt="" />
+                <h4 class="Follow">Follow Me</h4>
+                <img class="inst" src="https://i70.servimg.com/u/f70/20/33/45/13/group_10.png" alt="" />
+            </div>
+        </>
+    )
+}
